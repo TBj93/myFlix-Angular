93caf75ed2c83fb6d264868392374894b2645d57
Tim Burmester <Tim-Burmester1@web.de>
2023-01-04 23:00:50 +0100

    Changed apiURL
    had to switch to render, because of pricing of heroku

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -10,7 +10,7 @@ import { map } from 'rxjs/operators';
 
 
 
-const apiUrl = 'https://tims-movie-api.herokuapp.com/';
+const apiUrl = 'https://tims-movie-api.onrender.com/';
 @Injectable({
   providedIn: 'root'
 })
